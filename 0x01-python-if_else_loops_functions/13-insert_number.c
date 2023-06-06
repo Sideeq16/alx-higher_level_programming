574c50a14981209170f2b295eb3350325db10318
Sideeq16 <ajikuru3@gmail.com>
2023-06-06 21:25:52 +0000

    added file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,27 +2,40 @@
 #include <stdlib.h>
 #include "main.h"
 
-listint_t* insert_node(listint_t** head, int number) {
-    listint_t* new_node = (listint_t*)malloc(sizeof(listint_t));
-    if (new_node == NULL) {
-        return NULL;  // Failed to allocate memory for the new node
-    }
+/**
+ * insert_node - insert into a node
+ * @head: head
+ * @number: number to insert
+ * Return: new node
+ */
 
-    new_node->n = number;
-    new_node->next = NULL;
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
 
-    if (*head == NULL || number < (*head)->n) {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
+	if (new_node == NULL)
+	{
+		return (NULL);  /* Failed to allocate memory for the new node */
+	}
 
-    listint_t* current = *head;
-    while (current->next != NULL && number > current->next->n) {
-        current = current->next;
-    }
+	new_node->n = number;
+	new_node->next = NULL;
 
-    new_node->next = current->next;
-    current->next = new_node;
-    return new_node;
+	if (*head == NULL || number < (*head)->n)
+	{
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+	}
+
+	listint_t *current = *head;
+
+	while (current->next != NULL && number > current->next->n)
+	{
+	current = current->next;
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
 }
