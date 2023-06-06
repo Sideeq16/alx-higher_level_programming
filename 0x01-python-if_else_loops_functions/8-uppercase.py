1f89f51ba088c1845e1d390d6edef97fbf3d845c
Sideeq16 <ajikuru3@gmail.com>
2023-06-06 18:14:34 +0000

    added file

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(string):
+    for char in string:
+        uppercase_char = chr(ord(char) - ord('a') + ord('A')) \
+             if 'a' <= char <= 'z' else char
+        print(f"{uppercase_char}", end='')
+    print()
