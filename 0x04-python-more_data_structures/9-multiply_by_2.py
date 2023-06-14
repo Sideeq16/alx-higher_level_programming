f1e88a41dfc7c5bd40e53ec5de2a1e95dbc6b7d9
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-06-14 19:44:56 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 
 def multiply_by_2(a_dictionary):
     nw_dictionary = {}
-    for k,v in a_dictionary.items():
+    for k, v in a_dictionary.items():
         nw_dictionary[k] = v * 2
     return nw_dictionary
