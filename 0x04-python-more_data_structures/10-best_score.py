0a568b50b6a8d56e0d9b2e6e7e447c6c9525dd9d
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-06-14 19:56:48 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     maxi = 0
     store = "None"
     if a_dictionary:
-        for k,v in a_dictionary.items():
+        for k, v in a_dictionary.items():
             if a_dictionary[k] > maxi:
                 maxi = a_dictionary[k]
                 store = k
