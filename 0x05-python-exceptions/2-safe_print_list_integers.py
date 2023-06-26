ea59b0ae7980f491480c6a396bdad4131ce0afd5
Sideeq16 <ajikuru3@gmail.com>
2023-06-26 22:12:44 +0000

    added file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(elm, int):
                 print("{:d}".format(elm), end="")
                 count += 1
-    except (IndexError):
+    except (ValueError, TypeError):
         pass
 
     print()
