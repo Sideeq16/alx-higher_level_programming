05512b803b8644e6c34cb47fa5f6dea2f28dc5f3
Sideeq16 <ajikuru3@gmail.com>
2023-06-26 21:44:34 +0000

    added file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+    result = []
     for i in range(list_length):
-        result = 0
         try:
             el_1 = my_list_1[i]
             el_2 = my_list_2[i]
