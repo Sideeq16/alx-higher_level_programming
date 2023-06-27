da0c45b8e70b2116456757fce42240960ad88926
Sideeq16 <ajikuru3@gmail.com>
2023-06-27 02:08:21 +0000

    added file

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,35 +1,41 @@
 #include <Python.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void print_python_list(PyObject *p)
 {
+	Py_ssize_t size = PyList_Size(p);
+        Pyobject *item;
+        const char *type;
+
 	if (!PyList_Check(p))
 	{
 		fprintf(stderr, "Invalid PyListObject\n");
 		return;
 	}
 
-	Py_ssize_t size = PyList_Size(p);
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
 	for (Py_ssize_t i = 0; i < size; i++)
 	{
-		PyObject *item = PyList_GetItem(p, i);
-		const char *type = Py_TYPE(item)->tp_name;
+		item = PyList_GetItem(p, i);
+		type = Py_TYPE(item)->tp_name;
 		printf("Element %zd: %s\n", i, type);
 	}
 }
 
 void print_python_bytes(PyObject *p)
 {
+	Py_ssize_t size = PyBytes_Size(p);
+
 	if (!PyBytes_Check(p))
 	{
 		fprintf(stderr, "Invalid PyBytesObject\n");
 		return;
 	}
 
-	Py_ssize_t size = PyBytes_Size(p);
 	printf("[.] bytes object info\n");
 	printf("        - Length: %zd\n", size);
 	printf("        - Bytes: ");
@@ -47,6 +53,9 @@ void print_python_bytes(PyObject *p)
 
 void print_python_float(PyObject *p)
 {
+	
+	double value = PyFloat_AsDouble(p);
+
 	if (!PyFloat_Check(p))
 	{
 		fprintf(stderr, "Invalid PyFloatObject\n");
@@ -54,7 +63,5 @@ void print_python_float(PyObject *p)
 	}
 
 	printf("[.] float object info\n");
-
-	double value = PyFloat_AsDouble(p);
 	printf("        - Value: %f\n", value);
 }
