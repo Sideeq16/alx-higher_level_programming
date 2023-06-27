fa25523f7754b475f739ade2f683718ebf66082a
Sideeq16 <ajikuru3@gmail.com>
2023-06-27 01:12:27 +0000

    added file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         except TypeError:
-            print("wront type")
+            print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
         finally:
