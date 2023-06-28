434377a818cf4315941fef56c07b4f1aa50d5491
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-06-28 03:04:16 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,12 +46,44 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+    
+    @property
+    def position(self):
+        """Returns the position of the square field"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set the position of the square object
+        Args:
+            value (tuple): a tuple of two integers defining the position
+        Raises:
+            TypeError: when the value passed is not an integer or a two integer
+            tuplet
+            ValueError: when the value passed is less than 0
+        """
+        if type(value) != tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def my_print(self):
-        """print out the sqare with # symbol"""
-
-        if self.__size <= 0:
+        """print put the symbol #"""
+        if self.__size == 0:
             print()
-        else:
-            for i in range(self.__size):
-                print("#"*self.__size)
+            return
+
+        if self.__position[0] >= 0 and self.__position[1] >= 0:
+            for no_pos in range(self.__position[1]):
+                print()
+
+        for r in range(self.__size):
+            for spaces in range(self.__position[0]):
+                print(' ', end='')
+            for c in range(self.__size):
+                print('#', end='')
+            print()
