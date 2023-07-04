29f4ae8ad9b74e1c7872560973dd0736f7dca490
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-04 17:47:08 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Documentation for magic_string function"""
-
-
 def magic_string():
-    def helper(counter=[0]):
-        counter[0] += 1
-        return "BestSchool" * counter[0]
-    return helper
+    counter = [0]
+    return lambda: (counter.append(counter.pop() + 1), "BestSchool" * counter[0])[1]
