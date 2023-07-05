7e4c009dcee4feeecf55e181867b8527f3d2ad3b
Sideeq16 <ajikuru3@gmail.com>
2023-07-05 11:48:18 +0000

    added file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def magic_string():
-    counter = [0]
-    return lambda: (counter.append(counter.pop() + 1), "BestSchool" * counter[0])[1]
+    return ', '.join(["BestSchool] * (i + 1))
