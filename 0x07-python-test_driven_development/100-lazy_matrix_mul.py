5001dd70dcc24a6d6356f9bbed9b42b216ccadf8
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-06 03:56:10 +0100

    Update 100-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/100-lazy_matrix_mul.py
@@ -0,0 +1,12 @@
+import numpy as np
+"""Documentation for lazy_matrix_mul"""
+
+def lazy_matrix_mul(m_a, m_b):
+    """Documentation for function lazy_matrix_mul"""
+    try:
+        result = np.matmul(m_a, m_b)
+        return result.tolist()
+    except ValueError as e:
+        raise ValueError("Matrix multiplication failed. Ensure the matrices have compatible dimensions.") from e
+    except Exception as e:
+        raise Exception("An error occurred during matrix multiplication.") from e
