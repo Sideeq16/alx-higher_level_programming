5562c2502bb399704cee2cb91c1e6da14b5c5a61
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-10 17:22:34 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" List Class
-"""
+"""List Class"""
 
 
 class MyList(list):
-    """ Class that inherits from list
-    """
+    """Class that inherits from list"""
 
     def print_sorted(self):
-        """ Prints list in a sorted format
-        """
+        """Prints list in a sorted format"""
 
         new_list = self[:]
         new_list.sort()
