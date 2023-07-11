d061bb48687dfc45ae9f93c25ff2da06f2089e71
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-11 20:04:39 +0100

    Update 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module for converting from JSON
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ Converts a JSON string to a Python object
+    Args:
+        my_str (str): a JSON string to convert
+    """
+    return(json.loads(my_str))
