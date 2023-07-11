69ea7ec2fb70b32802a6d45ffa12b3fadb6dd440
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-11 17:13:46 +0100

    Update 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module fore reading a file """
+
+
+def read_file(filename=""):
+    """ Reads the file, filename, and prints to standard out
+    Args:
+        filename (str): The file to read
+    """
+    with open(filename, encoding="UTF-8") as f:
+        print(f.read(), end="")
