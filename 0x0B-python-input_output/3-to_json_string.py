c986082c6097b112e02ccb78e1586bfea479e50f
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-11 20:01:43 +0100

    Update 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Documentation for a function to_json_string
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ returns the JSON representation of an object (string)
+    Args:
+        my_obj (object): object ot turn into JSON
+    """
+    return(json.JSONEncoder().encode(my_obj))
