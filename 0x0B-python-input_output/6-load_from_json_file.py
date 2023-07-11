a917096aadae95d5374d00c62ee3c245546e4e18
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-11 20:10:06 +0100

    Update 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module to convert from JSON to Python
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """ function to create an Object from a JSON file
+    Args:
+        filename (str): filename to read from
+    """
+    with open(filename, "r", encoding="UTF-8") as f:
+        return(json.load(f))
