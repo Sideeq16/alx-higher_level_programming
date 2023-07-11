21bd9b697a7d1514668695a05e5e284c5f3e20fb
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-07-11 20:44:58 +0100

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,10 +5,14 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """ inserts a line of text to a file """
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-    with open(filename, 'w') as f:
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)  # Reset the file pointer to the beginning
+        
         for line in lines:
-            f.write(line)
+            file.write(line)  # Write the original line
+            
             if search_string in line:
-                f.write(new_string + '\n')
+                file.write(new_string + '\n')  # Write the new line after the matched line
+        
+        file.truncate()  # Remove any remaining content after the last line
